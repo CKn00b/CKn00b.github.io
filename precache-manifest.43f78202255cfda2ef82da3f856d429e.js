49f1cffe5a1a7baf5a997117946b6ce215a8bed9
ckn00b <cknew.dev@gmail.com>
2020-10-07 16:57:12 -0400

    Updates

---
+++ precache-manifest.43f78202255cfda2ef82da3f856d429e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb5bf43fa300c988b195e04392d2f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90c20a4f126854a2dcaa",
+    "url": "/static/css/main.e659c333.chunk.css"
+  },
+  {
+    "revision": "5e675ce4157447d5d959",
+    "url": "/static/js/2.9aeaa09b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9aeaa09b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c20a4f126854a2dcaa",
+    "url": "/static/js/main.d14233a3.chunk.js"
+  },
+  {
+    "revision": "83611b4122bc6d47a1fc",
+    "url": "/static/js/runtime-main.9a698b41.js"
+  }
+]);
